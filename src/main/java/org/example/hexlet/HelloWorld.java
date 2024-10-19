7d551ad07002d8e38fe27c149af97cf05af122b6
Artem <lunycat@mail.ru>
2024-10-20 01:03:28 +0300

    add unfinished jte

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,14 +1,33 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+import org.example.hexlet.dto.courses.CoursesPage;
+import org.example.hexlet.model.Course;
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
         Javalin app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
         });
 
+        app.get("/", ctx -> ctx.render("index.jte"));
+
+        app.get("/courses", ctx -> {
+            var courses = new ArrayList<>(List.of(
+                    new Course("Java", "Programming on Java"),
+                    new Course("PHP", "Программирование на PHP"),
+                    new Course("C++", "Программирование на C++")));
+            var header = "Курсы по программированию";
+            var page = new CoursesPage(courses, header);
+            ctx.render("courses/index.jte", model("page", page));
+        });
         app.get("/hello", ctx -> {
             String name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
             ctx.result("Hello " + name + "!");
